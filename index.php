5ec2ceba7413e5f7f34f9a15699c03467d3b58a8
CHRISTOPHER MOFFITT <Moffittchristopher@gmail.com>
2017-09-24 23:09:59 -0600

    some crap messing with java popup in index

--- index.php
+++ index.php
@@ -1,5 +1,18 @@
-<?php
+<center><b> " INDEX Page " </b> </center>
+<!doctype css>
 
-echo " hello  text";
-?>
+<!doctype html>
+<html>
+<head>
+	<!-- java script start -->
+<meta charset="UTF-8">
+<title>My Web Page</title>
+<script>
+  alert('annoying ass popup message');
+</script>
+<!-- java script end -->
+</head>
 
+<footer>
+
+</footer>

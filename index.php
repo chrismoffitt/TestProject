5dbe72c7134ca69b58fff4df7e4d3613b1005cf3
CHRISTOPHER MOFFITT <Moffittchristopher@gmail.com>
2017-09-25 12:27:42 -0600

    still cant get CSS to use external style sheets

--- index.php
+++ index.php
@@ -1,14 +1,11 @@
-<doctype css>
-
-
-
-<center><b> " INDEX Page " </b> </center>
+<doctype html>
 
 <html>
 <head>
-	
-<link rel="stylesheet" type="text/css" href="mystyle.css">
 
+	<center><b>  INDEX Page  </b> </center>
+
+<link rel="stylesheet" type="text/css" href="mystyle.css">
 
 <!-- java script start -->
 <meta charset="UTF-8">
@@ -17,8 +14,38 @@
   alert('annoying ass popup message');
 </script>
 <!-- java script end -->
+
+<style>
+#main {
+    width: 70px;
+    height: 300px;
+    border: 1px solid #c3c3c3;
+    display: -webkit-flex;
+    display: flex;
+    -webkit-flex-wrap: wrap;
+    flex-wrap: wrap;
+    -webkit-align-content: center;
+    align-content: center;
+}
+
+#main div {
+    width: 70px;
+    height: 70px;
+}
+</style>
 </head>
+<body>
 
-<footer>
+<div id="main">
+  <div style="background-color:coral;"></div>
+  <div style="background-color:lightblue;"></div>
+  <div style="background-color:pink;"></div>
+</div>
 
-</footer>
+<h2><b>  INDEX BODY CONTENT  </b></h2>
+
+<p><b>Note:</b>  this is how to include a note </p>
+	
+
+</body>
+</html>
